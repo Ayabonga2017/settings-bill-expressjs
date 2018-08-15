67e8390902aa923e19a0f066e94ca3372d0d916a
Ayabonga <nompetsheniayabonga@gmail>
2018-08-15 15:40:02 +0200

    added more passing test

--- test/settings-bill.test.js
+++ test/settings-bill.test.js
@@ -2,11 +2,25 @@ let assert = require("assert");
 const Settings = require('../settings-bill');
 let settings =Settings();
 
-describe('totalPhoneBill function', function(){
-  it('should Return R 2.75 for a sms', function(){
+describe('settings-bill function', function(){
+  it('should Return R 0.00 for a sms', function(){
 
-    var sms =settings.setSms(2.75);
-    assert.equal(sms,2.75)
+    var sms =settings.setSms(0.00);
+    assert.equal(sms,0.00)
   })
+    it('should Return R 2.75 for a calls', function(){
+  
+      var calls =settings.setCall(2.75);
+      assert.equal(calls,2.75)
+    })
 
+    it('should Return R 10.00 for warning', function(){
+      var setwarnining =settings.setwarnining(10.00);
+      assert.equal(setwarnining,10.00)
+    })
+
+    it('should Return R 40.00 for critical value', function(){
+      var setcritical =settings.setcritical(30.00);
+      assert.equal(setcritical,30.00)
+    })
 });
